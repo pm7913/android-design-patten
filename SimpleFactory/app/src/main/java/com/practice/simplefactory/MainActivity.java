4601753c1b3dbec346b8b9540c0e6d4cb5116bdf
Robert <pm7913@gmail.com>
2019-01-18 16:31:39 +0800

    test ssh

--- SimpleFactory/app/src/main/java/com/practice/simplefactory/MainActivity.java
+++ SimpleFactory/app/src/main/java/com/practice/simplefactory/MainActivity.java
@@ -25,5 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.message);
         textView.setText(builder.toString());
+
+        
     }
 }
